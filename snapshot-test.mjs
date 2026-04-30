4f63f45c4c2c7f948dc23116fc55fc3e80225db1
slash-naf <naf686833@gmail.com>
2026-04-30 19:01:04 +0900

    1件目の差分のみ表示

--- snapshot-test.mjs
+++ snapshot-test.mjs
@@ -8,36 +8,38 @@ const __dirname = path.dirname(__filename);
 
 const SNAPSHOT_FILE = path.join(__dirname, 'test-snapshot.json');
 
+const settingsList = [
+	{
+		magicianDifficulty: 'easy',
+		fastKnight: false,
+		fastDragon: false,
+		allowDragonStar: true,
+		hammerThrow: 1,
+		minIndex: 2800,
+		maxIndex: 3161,
+	},
+	{
+		magicianDifficulty: 'conservativeFast',
+		fastKnight: true,
+		fastDragon: true,
+		allowDragonStar: false,
+		hammerThrow: 1,
+		minIndex: 2800,
+		maxIndex: 3161,
+	},
+	{
+		magicianDifficulty: 'aggressiveFast',
+		fastKnight: true,
+		fastDragon: true,
+		allowDragonStar: false,
+		hammerThrow: 1,
+		minIndex: 2800,
+		maxIndex: 3161,
+	},
+];
+
 async function main() {
-	const results = [
-		{
-			magicianDifficulty: 'easy',
-			fastKnight: false,
-			fastDragon: false,
-			allowDragonStar: true,
-			hammerThrow: 1,
-			minIndex: 2800,
-			maxIndex: 3161,
-		},
-		{
-			magicianDifficulty: 'conservativeFast',
-			fastKnight: true,
-			fastDragon: true,
-			allowDragonStar: false,
-			hammerThrow: 1,
-			minIndex: 2800,
-			maxIndex: 3161,
-		},
-		{
-			magicianDifficulty: 'aggressiveFast',
-			fastKnight: true,
-			fastDragon: true,
-			allowDragonStar: false,
-			hammerThrow: 1,
-			minIndex: 2800,
-			maxIndex: 3161,
-		},
-	].map(v => {
+	const results = settingsList.map(v => {
 		const manipulator = new BattleWindowsMWWManipulator(v);
 		const a = [];
 		manipulator.test(3, ({name, result, index}) => {
@@ -52,18 +54,15 @@ async function main() {
 		console.log(`\nスナップショットを ${SNAPSHOT_FILE} で発見しました。比較中...`);
 		const oldData = JSON.parse(fs.readFileSync(SNAPSHOT_FILE, 'utf8'));
 
-		let diffFound = false;
-		for (let setting_index = 0; setting_index < oldData.length; setting_index++) {
-			for (let i = 0; i < oldData[setting_index].length; i++) {
-				const oldChunk = oldData[setting_index][i];
-				const newChunk = results[setting_index][i];
+		for (let settings_index = 0; settings_index < oldData.length; settings_index++) {
+			for (let i = 0; i < oldData[settings_index].length; i++) {
+				const oldChunk = oldData[settings_index][i];
+				const newChunk = results[settings_index][i];
 				const oldJson = JSON.stringify(oldChunk);
 				const newJson = JSON.stringify(newChunk);
 				if (oldJson !== newJson) {
-					diffFound = true;
-
 					console.error("❌ 現在のロジックとスナップショットの間に差異が見つかりました！");
-					console.log("setting_index = " + setting_index)
+					console.log(JSON.stringify(settingsList[settings_index]));
 
 					const length = Math.max(oldChunk.length, newChunk.length);
 					for (let i = 0; i < length; i++) {
@@ -75,12 +74,13 @@ async function main() {
 							console.log(oldJson +" -> "+ newJson);
 						}
 					}
+
+					process.exit(1);
 				}
 			}
 		}
 
-		if (diffFound) process.exit(1);
-		else console.log("✅ 差異は見つかりませんでした。結果はスナップショットと一致します。");
+		console.log("✅ 差異は見つかりませんでした。結果はスナップショットと一致します。");
 	} else {
 		console.log(`\nスナップショットが見つかりませんでした。初期結果を ${SNAPSHOT_FILE} に保存します...`);
 		fs.writeFileSync(SNAPSHOT_FILE, JSON.stringify(results));
